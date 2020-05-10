81002ed99d89fe2af21c7fd5903fddb1f31ce512
teratti-6 <t.m.t.k.m.s.k.n.k@gmail.com>
2020-05-10 19:42:14 +0900

    add11

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,10 +31,12 @@ set :linked_files, %w{ config/master.key }
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-  desc 'upload master.key'
+ task :restart do
+   invoke 'unicorn:restart'
+ end
+
+# 以下の記述により、GitHubに記録を残すことなく、master.keyを本番環境内に保存できる。
+ desc 'upload master.key'
   task :upload do
    on roles(:app) do |host|
     if test "[ ! -d #{shared_path}/config ]"
